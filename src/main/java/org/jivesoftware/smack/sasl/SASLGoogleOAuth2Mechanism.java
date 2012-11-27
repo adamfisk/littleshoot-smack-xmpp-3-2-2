d093325e45dbc6bd2711a0000aaa68ce976fd14c
Adam Fisk <a@littleshoot.org>
2012-11-26 19:56:14 -0800

    Better exception handling

--- src/main/java/org/jivesoftware/smack/sasl/SASLGoogleOAuth2Mechanism.java
+++ src/main/java/org/jivesoftware/smack/sasl/SASLGoogleOAuth2Mechanism.java
@@ -2,10 +2,17 @@ package org.jivesoftware.smack.sasl;
 
 import java.io.IOException;
 
-import javax.security.auth.callback.Callback;
 import javax.security.auth.callback.CallbackHandler;
 import javax.security.auth.callback.TextInputCallback;
 import javax.security.auth.callback.UnsupportedCallbackException;
+
+import org.apache.commons.lang.StringUtils;
+import org.jivesoftware.smack.SASLAuthentication;
+import org.jivesoftware.smack.XMPPException;
+import org.jivesoftware.smack.packet.Packet;
+import org.jivesoftware.smack.util.Base64;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.google.api.client.auth.oauth2.ClientParametersAuthentication;
 import com.google.api.client.auth.oauth2.RefreshTokenRequest;
@@ -15,14 +22,11 @@ import com.google.api.client.http.GenericUrl;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.jackson.JacksonFactory;
 
-import org.jivesoftware.smack.packet.Packet;
-import org.jivesoftware.smack.SASLAuthentication;
-import org.jivesoftware.smack.util.Base64;
-import org.jivesoftware.smack.XMPPException;
-
 
 public class SASLGoogleOAuth2Mechanism extends SASLMechanism {
 
+    private final Logger log = LoggerFactory.getLogger(getClass());
+    
     private String clientID;
     private String clientSecret;
 
@@ -33,17 +37,20 @@ public class SASLGoogleOAuth2Mechanism extends SASLMechanism {
         super(sa);
     }
 
+    @Override
     protected String getName() {
         return "X-OAUTH2";
     }
 
     @Override
-    public void authenticate(String username, String password, String host) throws IOException, XMPPException {
+    public void authenticate(String username, String pass, String host) 
+        throws IOException, XMPPException {
         throw new XMPPException("Google doesn't support password authentication in OAuth2.");
     }
 
     @Override
-    public void authenticate(String username, String host, CallbackHandler cbh) throws IOException, XMPPException {
+    public void authenticate(String username, String host, CallbackHandler cbh) 
+        throws IOException, XMPPException {
 
         //Set the authenticationID as the username, since they must be the same
         //in this case.
@@ -51,15 +58,14 @@ public class SASLGoogleOAuth2Mechanism extends SASLMechanism {
         this.hostname = host;
 
         // DRY warning: these must match the indices below.
-        TextInputCallback[] cbs = {new TextInputCallback("clientID"),
+        final TextInputCallback[] cbs = {new TextInputCallback("clientID"),
                                    new TextInputCallback("clientSecret"),
                                    new TextInputCallback("accessToken"),
                                    new TextInputCallback("refreshToken")};
         try {
             cbh.handle(cbs);
-        } catch (UnsupportedCallbackException e) {
-            e.printStackTrace();
-            throw new IOException("UnsupportedCallback: " + e.getMessage());
+        } catch (final UnsupportedCallbackException e) {
+            throw new IOException("UnsupportedCallback", e);
         }
         // DRY warning: these must match the order in the array above.
         clientID = cbs[0].getText();
@@ -75,12 +81,13 @@ public class SASLGoogleOAuth2Mechanism extends SASLMechanism {
 
         refreshAccessToken();
 
-        String raw = "\0" + this.authenticationId + "\0" + this.accessToken;
+        final String raw = "\0" + this.authenticationId + "\0" + this.accessToken;
         final String authenticationText = Base64.encodeBytes(
                 raw.getBytes("UTF-8"),
                 Base64.DONT_BREAK_LINES);
         // Send the authentication to the server
         getSASLAuthentication().send(new Packet() {
+            @Override
             public String toXML() {
                 return "<auth mechanism=\"X-OAUTH2\""
                         + " auth:service=\"oauth2\""
@@ -92,32 +99,32 @@ public class SASLGoogleOAuth2Mechanism extends SASLMechanism {
         });
     }
 
-    private void refreshAccessToken() {
+    private void refreshAccessToken() throws IOException {
+        log.info("Refreshing token with refresh: "+refreshToken+
+            "\nclientId: "+clientID+
+            "\nclientSecret:"+clientSecret);
         try {
-            TokenResponse response =
-                new RefreshTokenRequest(new NetHttpTransport(), new JacksonFactory(), new GenericUrl(
-                            "https://accounts.google.com/o/oauth2/token"), refreshToken)
-                    .setClientAuthentication(new ClientParametersAuthentication(clientID, clientSecret))
+            final TokenResponse response =
+                new RefreshTokenRequest(new NetHttpTransport(), 
+                    new JacksonFactory(), new GenericUrl(
+                        "https://accounts.google.com/o/oauth2/token"), refreshToken)
+                    .setClientAuthentication(new ClientParametersAuthentication(
+                        clientID, clientSecret))
                     .execute();
-            String token = response.getAccessToken();
-            if (token != null) {
-                System.out.println("Got new access token: " + token);
+            final String token = response.getAccessToken();
+            if (StringUtils.isNotBlank(token)) {
+                log.info("Got new access token!!");
                 accessToken = token;
-            }
-        } catch (TokenResponseException e) {
-            if (e.getDetails() != null) {
-                System.err.println("Error: " + e.getDetails().getError());
-                if (e.getDetails().getErrorDescription() != null) {
-                    System.err.println(e.getDetails().getErrorDescription());
-                }
-                if (e.getDetails().getErrorUri() != null) {
-                    System.err.println(e.getDetails().getErrorUri());
-                }
             } else {
-                System.err.println(e.getMessage());
+                log.error("Retreived null access token in: {}", response);
+                throw new IOException("Retreived null token?\n"+response);
             }
-        } catch (IOException e) {
-            System.err.println("IO exception while trying to refresh token --ignoring.");
+        } catch (final TokenResponseException e) {
+            log.error("Token error -- maybe revoked or unauthorized?", e);
+            throw new IOException("Problem with token -- maybe revoked?", e);
+        } catch (final IOException e) {
+            log.warn("IO exception while trying to refresh token.", e);
+            throw e;
         }
     }
 }
